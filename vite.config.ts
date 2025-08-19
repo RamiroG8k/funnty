df71c3e966adb2ea469ad0c41c8e29b79c125110
Ramiro M <ramiro.g8k@gmail.com>
2025-08-19 16:17:11 -0600

    Init

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: "prompt",
+            injectRegister: false,
+
+            pwaAssets: {
+                disabled: false,
+                config: true,
+            },
+
+            manifest: {
+                name: "Funnty",
+                short_name: "Funnty",
+                description:
+                    "Fun app to create text elements as images for stickers of creative media content",
+                theme_color: "#702632",
+            },
+
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
+
+            devOptions: {
+                enabled: false,
+                navigateFallback: "index.html",
+                suppressWarnings: true,
+                type: "module",
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
