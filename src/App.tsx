25604fb83496a57448649bfd49086f718061e623
Ramiro M <ramiro.g8k@gmail.com>
2025-08-23 13:13:14 -0600

    Add PWA badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import PWABadge from "./PWABadge";
 import { Home } from "./views/Home";
 
 const App: React.FC = () => {
@@ -6,6 +7,8 @@ const App: React.FC = () => {
             <div className="h-full w-full bg-gradient-to-br from-[#FF0000] via-transparent to-[#FF0000] absolute z-[0] backdrop-blur-xl opacity-5" />
 
             <Home />
+
+            <PWABadge />
         </>
     );
 };
